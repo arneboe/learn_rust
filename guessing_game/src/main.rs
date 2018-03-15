2a9c7bfdc989eb5d04799f9cdfd2bcd7cc068b9e
Arne Böckmann <arne.boeckmann@dfki.de>
2018-03-15 14:02:46 +0100

    guessing game works

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,34 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input Guess");
+
+    let secret_num: i32 = rand::thread_rng().gen_range(0, 100);
+
+    loop {
+        let mut guess: String = String::new();
+        let bytes_read = io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        //parse to int and shadow variable
+        //trim also removes the '\n'
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("secret_num {}", secret_num);
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("equal");
+                break;
+            }
+        }
+    }
 }
